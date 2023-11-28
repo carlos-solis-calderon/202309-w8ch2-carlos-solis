6c6f1105b940b5481e3840c02aaf95104a530a2a
Carlos Solis <mrsoliscalderon@gmail.com>
2023-11-28 18:13:48 +0100

    finish scafolding and add structuring

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,37 +1,52 @@
-import { NextFunction, Request, Response } from 'express';
-import createDebug from 'debug';
-import { UsersMongoRepo } from '../repos/users/users.mongo.repo.js';
-import { Auth } from '../services/auth.js';
-import { User } from '../entities/user.js';
-import { Controller } from './controller.js';
+// Import { NextFunction, Request, Response } from 'express';
+// import createDebug from 'debug';
+// import { UsersMongoRepo } from '../repos/users/users.mongo.repo.js';
+// import { Auth } from '../services/auth.js';
+// import { User } from '../entities/user.js';
+// import { Controller } from './controller.js';
+// import { LoginResponse } from '../types/login.response.js';
+// import { HttpError } from '../types/http.error.js';
 
-const debug = createDebug('W8E:users:controller');
+// const debug = createDebug('W8E:users:controller');
 
-export class UsersController extends Controller<User> {
-  // eslint-disable-next-line no-unused-vars
-  constructor(protected repo: UsersMongoRepo) {
-    super(repo);
-    debug('Instantiated');
-  }
+// export class UsersController extends Controller<User> {
+//   cloudinaryService: any;
+//   // eslint-disable-next-line no-unused-vars
+//   constructor(protected repo: UsersMongoRepo) {
+//     super(repo);
+//     debug('Instantiated');
+//   }
 
-  async login(req: Request, res: Response, next: NextFunction) {
-    try {
-      const result = req.body.userId
-        ? await this.repo.getById(req.body.userId)
-        : await this.repo.login(req.body);
+//   async login(req: Request, res: Response, next: NextFunction) {
+//     try {
+//       const result = req.body.userId
+//         ? await this.repo.getById(req.body.userId)
+//         : await this.repo.login(req.body);
 
-      const data = {
-        user: result,
-        token: Auth.signJWT({
-          id: result.id,
-          email: result.email,
-        }),
-      };
-      res.status(202);
-      res.statusMessage = 'Accepted';
-      res.json(data);
-    } catch (error) {
-      next(error);
-    }
-  }
-}
+//       const data: LoginResponse = {
+//         user: result,
+//         token: Auth.signJWT({
+//           id: result.id,
+//           email: result.email,
+//         }),
+//       };
+//       res.status(202);
+//       res.statusMessage = 'Accepted';
+//       res.json(data);
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+
+//   async create(req: Request, res: Response, next: NextFunction) {
+//     try {
+//       if (!req.file)
+//         throw new HttpError(406, 'Not Acceptable', 'Invalid multer file');
+//       const imgData = await this.cloudinaryService.uploadImage(req.file.path);
+//       req.body.avatar = imgData;
+//       super.create(req, res, next);
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// }
